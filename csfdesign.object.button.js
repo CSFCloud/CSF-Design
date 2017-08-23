7bbcb390cd6baa1859b30b1d47780bf7e30f9251
Bence Skorka <skorka.bence@gmail.com>
2017-08-23 13:21:23 +0200

    added more button styles

--- csfdesign.object.button.js
+++ csfdesign.object.button.js
@@ -10,6 +10,12 @@ csfdesign.define("csf/object/button", {
         if (e.hasAttribute("data-csftheme-button-color")) {
             c = e.getAttribute("data-csftheme-button-color");
         }
+
+        var reversed = false;
+        if (e.hasAttribute("data-csftheme-button-reverse")) {
+            reversed = true;
+        }
+
         if (style == "link") {
             var brd = document.createElement("div");
             brd.className = "_border";
@@ -26,8 +32,16 @@ csfdesign.define("csf/object/button", {
         }
         if (style == "raised") {
             $(e).addClass("_button-raised");
-            $(e).css("background-color", c);
+            if (reversed) {
+                $(e).css("color", c);
+                $(e).css("background-color", "white");
+                $(e).css("font-weight", "bold");
+            } else {
+                $(e).css("color", "white");
+                $(e).css("background-color", c);
+            }
         }
+        $(e).addClass("_disable-select");
         e.removeAttribute("data-csftheme-button-style");
     }
 });
